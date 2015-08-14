cce82cd65edfb2a9aa01c55037615e7891b0e0c9
Josh Leverette <coder543@gmail.com>
2015-08-14 00:05:54 -0500

    (Untested) finished the predict function.

--- kalman.cpp
+++ kalman.cpp
@@ -1,9 +1,30 @@
 #include "kalman.h"
-#include "mbed.h"
+#include "arm_math.h"
+
+#define arm_mat_instance(val) { \
+                                  .numRows = num_vars, \
+                                  .numCols = num_vars, \
+                                  .pData = val \
+                              }
+
+#define arm_mat_instance_col(val) { \
+                                  .numRows = num_vars, \
+                                  .numCols = 1, \
+                                  .pData = val \
+                              }
+
+typedef arm_matrix_instance_f32 arm_matrix;
 
 kalman::kalman()
 {
 
+    /* we need to transpose F and store it in F_T for future use */
+    arm_matrix mat_F = arm_mat_instance(F);
+
+    arm_matrix mat_F_T = arm_mat_instance(F_T);
+
+    arm_mat_trans_f32(&mat_F, &mat_F_T);
+    /* F_T now holds the transposed F */
 }
 
 kalman::~kalman()
@@ -13,9 +34,45 @@ kalman::~kalman()
 
 void kalman::predict()
 {
-	//x = Fx
-	//P = FP(F^T) + Q
-	arm_matrix_instance_f32 test;
+
+    float new_X[num_vars];
+
+    arm_matrix mat_new_X = arm_mat_instance_col(new_X);
+    arm_matrix mat_current_X = arm_mat_instance_col(X);
+    arm_matrix mat_F = arm_mat_instance(F);
+
+                /*** x = F * x ***/
+
+    //new_X = F * current_X
+    arm_mat_mult_f32(&mat_F, &mat_current_X, &mat_new_X);
+
+    // X = new_X;
+    memcpy(X, new_X, sizeof(float) * num_vars);
+
+
+    float new_P[num_vars * num_vars];
+
+    arm_matrix mat_new_P = arm_mat_instance(new_P);
+    arm_matrix mat_current_P = arm_mat_instance(P);
+    arm_matrix mat_F_T = arm_mat_instance(F_T);
+    arm_matrix mat_Q = arm_mat_instance(Q);
+
+
+            /*** P = F * P * (F^T) + Q ***/
+
+    //new_P = F * current_P
+    arm_mat_mult_f32(&mat_F, &mat_current_P, &mat_new_P);
+
+    //current_P = new_P * F^T
+    arm_mat_mult_f32(&mat_new_P, &mat_F_T, &mat_current_P);
+
+    //new_P = current_P + Q
+    arm_mat_add_f32(&mat_current_P, &mat_Q, &mat_new_P);
+
+    // P = new_P;
+    memcpy(P, new_P, sizeof(float) * num_vars * num_vars);
+
+
 }
 
 void kalman::update(float measured)
--- kalman.h
+++ kalman.h
@@ -3,12 +3,13 @@
 
 const float dt   = 0.000625;
 const float dt_2 = 0.000000390625;
+const int num_vars = 3;
 
 class kalman
 {
 public:
     /*** current state ***/
-    float X[3] = {
+    float X[num_vars] = {
                     0,  //position
                     0,  //velocity
                     0   //acceleration
@@ -16,30 +17,32 @@ public:
 
 private:
     /*** values for prediciton step ***/
-    float P[3][3] = { //covariance matrix
-                        {0,   0,   0},
-                        {0, 100,   0},
-                        {0,   0, 500}
+    float P[num_vars * num_vars] = { //covariance matrix
+                        0,   0,   0,
+                        0, 100,   0,
+                        0,   0, 500
                     };
 
-    float F[3][3] = { //state transition function
-                        {1,  dt, 0.5 * dt_2},
-                        {0,   1,     dt    },
-                        {0,   0,     1     }
+    float F[num_vars * num_vars] = { //state transition function
+                        1,  dt, 0.5 * dt_2,
+                        0,   1,     dt    ,
+                        0,   0,     1     
                     }; 
 
-    float Q[3][3] = { //system noise
-                        {0,   0,   0},
-                        {0,   0,   0},
-                        {0,   0,   0}
+    float F_T[num_vars * num_vars]; //state transition function, transposed
+
+    float Q[num_vars * num_vars] = { //system noise
+                        0,   0,   0,
+                        0,   0,   0,
+                        0,   0,   0
                     };
 
 
     /*** values for update step ***/
-    float H[3] = {0, 0, 16384}; //state space to measurement space conversion
+    float H[num_vars] = {0, 0, 16384}; //state space to measurement space conversion
     float R = 0.01; //sensor measurement noise
-    float S[3][3]; //system uncertainty
-    float K[3][3]; //kalman gain
+    float S[num_vars * num_vars]; //system uncertainty
+    float K[num_vars * num_vars]; //kalman gain
 
 
 public:
